2dd39b5e1d2057282531455341632a1bb53c12a9
xuhui <zmm064@foxmail.com>
2018-04-18 10:48:21 +0800

    创建自定义模板标签

--- boards/tests/test_views.py
+++ boards/tests/test_views.py
@@ -8,9 +8,9 @@ from django.urls import resolve
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
